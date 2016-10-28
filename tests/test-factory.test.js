29cb47187d027092e092f04f6c966cf7790ffaa7
Gary Chisholm <garyjchisholm@gmail.com>
2016-10-28 17:47:40 +1100

    Update: Tests and dependencies

--- tests/test-factory.test.js
+++ tests/test-factory.test.js
@@ -1,20 +1,20 @@
 describe("TestFactoryTester", function() {
     var f = TestFactory.testObject;
- 
+
     beforeEach(function(){
-        spyOn(f, 'init').andCallThrough();
+        spyOn(f, 'init').and.callThrough();
     });
- 
+
     afterEach(function() {
         f.reset();
     });
- 
+
     it("should be able to initialize", function() {
         expect(f.init).toBeDefined();
         f.init();
         expect(f.init).toHaveBeenCalled();
     });
- 
+
     it("should initialise an empty object", function(){
         f.init();
         expect(typeof f.obj).toEqual("object");
@@ -42,5 +42,24 @@ describe("TestFactoryTester", function() {
 
         expect(f.isEmpty()).toEqual(true);
         expect(f.obj.firstName).toBeUndefined();
+    });
+
+    it("should add and remove nested objects", function(){
+        f.init();
+        f.add("company", {
+            name: "Acme",
+            location: "London"
+        });
+
+        expect(f.isEmpty()).toEqual(false);
+        expect(f.obj.company).toBeDefined();
+        expect(f.obj.company.name).toBeDefined();
+        expect(f.obj.company.name).toEqual("Acme");
+        expect(f.obj.company.location).toEqual("London");
+
+        f.remove("company");
+
+        expect(f.isEmpty()).toEqual(true);
+        expect(f.obj.company).toBeUndefined();
     });
 });
