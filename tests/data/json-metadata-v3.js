69e8864c4ad3226c4def56adb265576b53931f4a
Gary Chisholm <omgaz@users.noreply.github.com>
2019-07-23 11:01:49 +0100

    [patch] update metadata

---
+++ tests/data/json-metadata-v3.js
@@ -0,0 +1,37 @@
+module.exports = {
+  version: 3,
+  created: '2019-07-23T09:36:20.719Z',
+  modified: '2019-07-23T09:39:25.577Z',
+  assetUrl: 'stuff/Appicon-readme.png',
+  type: 'image/png',
+  description: 'Readme icon for git repo',
+  author: {
+    id: 14,
+    name: 'Gary',
+  },
+  keywords: ['app logo', 'logo', 'readme'],
+  status: 'approved',
+  versionHistory: {
+    1: {
+      modified: '2019-07-23T09:36:20.719Z',
+      assetUrl: 'readme.svg',
+      type: 'image/svg+xml',
+      author: {
+        id: 14,
+        name: 'Gary',
+      },
+      keywords: ['app logo', 'logo', 'readme'],
+    },
+    2: {
+      modified: '2019-07-23T09:36:20.719Z',
+      assetUrl: 'stuff/Appicon-readme.svg',
+      type: 'image/svg+xml',
+      description: 'Readme icon for git repo',
+      author: {
+        id: 14,
+        name: 'Gary',
+      },
+      keywords: ['app logo', 'logo', 'readme'],
+    }
+  }
+}
--- tests/performance.js
+++ tests/performance.js
@@ -1,80 +1,10 @@
 const benchmarker = require('./benchmarker');
 const diffler = require('../src');
+const metadatav3 = require('./data/json-metadata-v3');
+const metadatav2 = require('./data/json-metadata-v2');
 
 const suite = function() {
-  const testObjectA = {
-    "id": 157538,
-    "date": "2017-07-21T10:30:34",
-    "date_gmt": "2017-07-21T17:30:34",
-    "guid": {
-      "rendered": "https://www.sitepoint.com/?p=157538"
-    },
-    "modified": "2017-07-23T21:56:35",
-    "modified_gmt": "2017-07-24T04:56:35",
-    "slug": "why-the-iot-threatens-your-wordpress-site-and-how-to-fix-it",
-    "status": "publish",
-    "type": "post",
-    "link": "https://www.sitepoint.com/why-the-iot-threatens-your-wordpress-site-and-how-to-fix-it/",
-    "title": {
-      "rendered": "Why the IoT Threatens Your WordPress Site (and How to Fix It)"
-    },
-    "content": {
-    },
-    "excerpt": {
-    },
-    "author": 72546,
-    "featured_media": 157542,
-    "comment_status": "open",
-    "ping_status": "closed",
-    "sticky": false,
-    "template": "",
-    "format": "standard",
-    "meta": [],
-    "categories": [
-      6132
-    ],
-    "tags": [
-      1798,
-      6298
-    ]
-  };
-  const testObjectB = {
-    "id": 157538,
-    "date": "2017-07-21T10:30:34",
-    "date_gmt": "2017-07-21T17:30:34",
-    "guid": {
-      "rendered": "https://www.sitepoint.com/?p=157538"
-    },
-    "modified": "2017-07-23T21:56:35",
-    "modified_gmt": "2017-07-24T04:56:35",
-    "slug": "why-the-iot-threatens-your-wordpress-site-and-how-to-fix-it",
-    "status": "publish",
-    "type": "post",
-    "link": "https://www.sitepoint.com/why-the-iot-threatens-your-wordpress-site-and-how-to-fix-it/",
-    "title": {
-      "rendered": "Why the IoT Threatens Your WordPress Site (and How to Fix It)"
-    },
-    "content": {
-    },
-    "excerpt": {
-    },
-    "author": 72546,
-    "featured_media": 157542,
-    "comment_status": "open",
-    "ping_status": "closed",
-    "sticky": false,
-    "template": "",
-    "format": "standard",
-    "meta": [],
-    "categories": [
-      6132
-    ],
-    "tags": [
-      1798,
-      6298
-    ]
-  };
-  diffler(testObjectA, testObjectB);
+  diffler(metadatav2, metadatav3);
 }
 
 const ops = benchmarker.bench10(suite);
